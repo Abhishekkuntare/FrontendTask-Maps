0b3920b7a5e15e2415103c691b1765686879e01d
Abhishek-Kuntare <abhishek@levitation.co.in>
2024-05-19 22:36:52 +0530

    add font

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,14 +4,20 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Maps - Search the country,city,user ",
+  title: "Maps - Search the country, city, user",
   description:
-    "This is the maps app that shows the country,city whatever you search",
+    "This is the maps app that shows the country, city whatever you search",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Hachi+Maru+Pop&family=Mochiy+Pop+One&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
